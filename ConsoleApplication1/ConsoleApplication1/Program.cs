0b6b6223c6d6315812c8a4223f332bd2894d6b85
Naveen <mnaveinkumar@gmail.com>
2016-02-16 07:58:46 +0530

    Simple recursion

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Type a number to get tables from 2 till ur number!");
+            int j = int.Parse(Console.ReadLine());
+            Program prg = new Program();
+
+            prg.meth(j);
+        }
+
+        public void meth(int i)
+        {
+
+            if (i != 1)
+            {
+                meth(i - 1);
+                Console.WriteLine("----------------");
+                for (int k = 1; k <= 10; k++)
+                {
+
+                    Console.WriteLine(i + "X" + k + "=" + k * i);
+                }
+            }
+
+
+        }
+    }
+}
